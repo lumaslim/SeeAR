7a92caeb90a01f16e460ced35197e2766718611f
SLimma <30815731+slimlime@users.noreply.github.com>
2018-12-13 20:50:34 +1000

    Attempt build phase script file instead of embedded for build versioning

---
+++ SeeAR/Scripts/buildVersionIncrementer.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#  Script.sh
+#  TDDPlaces
+#
+#  Created by SLim on 12/12/18.
+#  Copyright © 2018 SLim. All rights reserved.
+#!/bin/bash
+#branch=${1:-'master'}
+#buildNumber=$(expr $(git rev-list $branch --count) - $(git rev-list HEAD..$branch --count))
+#echo "Updating build number to $buildNumber using branch '$branch'."
+#/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
+#if [ -f "${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}.dSYM/Contents/Info.plist" ]; then
+#/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}.dSYM/Contents/Info.plist"
+#fi
+
+buildNumber=$(git rev-list --count HEAD)
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "$PRODUCT_SETTINGS_PATH"
+# Use Build run script phase in project rather than bot. Xcode Server pre-integration hook in Xcode 10 no longer has access to the same environment variables and build resources? Changes in build steps.
